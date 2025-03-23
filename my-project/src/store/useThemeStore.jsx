6a0b970a66d72eb73f2568272f339e4f580bdf20
koulachyut <koulachyut@gmail.com>
2025-03-23 16:57:55 +0530

    first

--- my-project/src/store/useThemeStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-const useThemeStore= create((set,get)=>({
-    theme: localStorage.getItem("theme") || "lemonade",
-    setTheme:(theme)=>{
-        set({theme:theme})
-        localStorage.setItem("theme",theme)
-    },
-}));
-export default useThemeStore;
