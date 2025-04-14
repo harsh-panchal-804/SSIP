dfad27c925bba15c75cb7444a351c0d5e3a7cef1
koulachyut <koulachyut@gmail.com>
2025-04-14 14:39:19 +0530

    Update user.schema.js

--- Backend/model/Schema/user.schema.js
+++ Backend/model/Schema/user.schema.js
@@ -23,11 +23,63 @@ const userSchema = new mongoose.Schema({
         enum: ["admin", "officer", "user"],
         default: "user"
     },
+    refreshToken:{
+        type:String
+    },
+    profilePicture: {
+        type: String,
+        required:true
+    },
+    twoFactorToken:{
+        type:String,//Base32 format 
+    },
+    otpExpiry:{
+        type:Date
+    },
+     otpRequest:{
+     type:[Date],
+     default:[]
+    },
 }, {
     timestamps: {
         createdAt: true,
         updatedAt: false
     }
 });
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
 
-export default mongoose.model("userModel", userSchema);
+    this.password= await bcrypt.hash(this.password,10);
+    next();
+})
+ userSchema.methods.isPasswordCorrect= async function (password) {
+    return await bcrypt.compare(password,this.password)
+ }
+ userSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            firstName:this.firstName,
+            email:this.email,
+            username:this.username,
+            role:this.role
+        },
+        process.env.ACCESS_TOKEN,
+        {
+            expiresIn: process.env.ACCESS_EXPIRY
+        }
+    )
+ }
+ userSchema.methods.genrateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN,
+        {
+            expiresIn:process.env.REFRESH_EXPIRY
+        }
+    )
+ }
+
+export default mongoose.model("User", userSchema);
